559db7bed5be1bf0a89b4c3728c56f72734e3975
ZentaGoran <goran.backlund@zenta.se>
2019-03-29 17:09:42 +0100

    first test

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    public float xAngle, yAngle, zAngle;
+    public Material selfMat, worldMat;
+    private GameObject sphere1, sphere2;
+
+    void Start()
+    {
+        sphere1 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere1.transform.position = new Vector3(0.75f, 0.0f, 0.0f);
+        sphere1.GetComponent<Renderer>().material = selfMat;
+        sphere1.name = "Sun";
+
+        sphere2 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere2.transform.position = new Vector3(-0.75f, 0.0f, 0.0f);
+        sphere2.GetComponent<Renderer>().material = worldMat;
+        sphere2.name = "Planet";
+
+    }
+
+    void Update()
+    {
+        sphere2.transform.Rotate(xAngle, yAngle, zAngle, Space.World);
+
+    }
+}
