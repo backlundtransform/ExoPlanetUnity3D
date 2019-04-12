0baa530828f61d35131964a2438bb63e610c6fdd
gbanm <gbanm06@gmail.com>
2019-04-12 23:39:32 +0200

    update test

---
+++ Assets/Script/RotatePlanet.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class RotatePlanet : MonoBehaviour
+{
+    public float xAngle, yAngle, zAngle;
+    private GameObject _sun, _planet;
+    public Material selfMat, worldMat;
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+        _sun = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        _sun.transform.position = new Vector3(14.22f, 7.11f, 40f);
+        _sun.transform.localScale = new Vector3(20, 20, 20);
+        _sun.name = "Sun";
+    
+        _planet = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        _planet.GetComponent<Renderer>().material = selfMat;
+        _planet.transform.position = new Vector3(4.22f, 4f, 0f);
+        _sun.transform.localScale = new Vector3(2, 2, 2);
+        _planet.name = "Earth";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        _planet.transform.RotateAround(_sun.transform.localPosition, Vector3.up, Time.deltaTime);
+    }
+
+
+
+}
