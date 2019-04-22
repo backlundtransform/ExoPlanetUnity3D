6ad58ac0398d017be37a40dcc31bb5c601a049f9
gbanm <gbanm06@gmail.com>
2019-04-22 20:22:52 +0200

    moving

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -2,22 +2,47 @@
 
 public class Player : MonoBehaviour
 {
-    public float speed;
+    // Adjust the speed for the application.
+    public float speed = 1.0f;
+    public float step = 1.0f;
 
-    private Rigidbody rb;
+    // The target (cylinder) position.
+    public Transform target;
 
-    void Start()
+    void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        // Position the cube at the origin.
+        transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+
+        // Create and position the cylinder. Reduce the size.
+        GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        Camera.main.transform.position = new Vector3(0.85f, 1.0f, -3.0f);
+
+        // Grab cylinder values and place on the target.
+        target = cylinder.transform;
+        target.transform.localScale = new Vector3(0.15f, 1.0f, 0.15f);
+        target.transform.position = new Vector3(0.8f, 0.0f, 0.8f);
+
+        // Position the camera.
+        Camera.main.transform.position = new Vector3(0.85f, 1.0f, -3.0f);
+        Camera.main.transform.localEulerAngles = new Vector3(15.0f, -20.0f, -0.5f);
+
+        // Create and position the floor.
+        GameObject floor = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        floor.transform.position = new Vector3(0.0f, -1.0f, 0.0f);
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+     
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Camera.main.transform.position = new Vector3(0.85f, 1.0f, step++);
+          
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+            target.position  = new Vector3(0.0f, 0.0f, step++);
 
-        rb.AddForce(movement * speed);
+        }
+     
     }
 }
