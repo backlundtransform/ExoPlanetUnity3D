3f46e35fc48ede2472341d89e9051ce63fda24a6
gbanm <gbanm06@gmail.com>
2019-05-05 01:25:09 +0200

    InputAccepter

---
+++ Assets/Script/InputAccepter.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class InputAccepter : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public float zstep = 1.0f;
+    public float xstep = 1.0f;
+    public float ystep = 1.0f;
+    public GameObject world;
+    private void Awake()
+    {
+        Player.onTriggerDown += TriggerDown;
+        Player.onTuchpadDown += TriggerDown;
+        Player.onTuchpadUp += TriggerUp;
+    }
+
+    void Start()
+    {
+        world = GameObject.FindWithTag("Player");
+        world.transform.position = new Vector3(xstep, ystep, zstep);
+    }
+
+    void TriggerDown()
+    {
+        world = GameObject.FindWithTag("Player");
+        world.transform.position = new Vector3(xstep, ystep, zstep++);
+    }
+    void TriggerUp()
+    {
+        world = GameObject.FindWithTag("Player");
+        world.transform.position = new Vector3(xstep, ystep, zstep--);
+    }
+}
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,43 +1,96 @@
 ﻿using UnityEngine;
 using UnityEngine.XR;
+using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
-
+    public static UnityAction<bool> onHasController = null;
+    public static UnityAction  onTriggerDown = null;
+    public static UnityAction onTriggerUp= null;
+    public static UnityAction onTuchpadUp = null;
+    public static UnityAction onTuchpadDown = null;
+    private bool hasController = false;
     public float speed = 1.0f;
     public float zstep = 1.0f;
     public float xstep = 1.0f;
     public float ystep = 1.0f;
     public GameObject world;
-  
+    private bool inputActive = true;
+    public Transform controller;
+    public static bool leftHanded { get; private set; }
+    System.IO.StreamWriter recording;
 
+    private void Awake()
+    {
+        OVRManager.HMDMounted += PlayerFound;
+        OVRManager.HMDUnmounted += PlayerLost;
+    }
+
+    private void OnDestroy()
+    {
+        OVRManager.HMDMounted -= PlayerFound;
+        OVRManager.HMDUnmounted -= PlayerLost;
+    }
 
     void Start()
     {
 
-
+        leftHanded = OVRInput.GetControllerPositionTracked(OVRInput.Controller.LTouch);
         world = GameObject.FindWithTag("Player");
         world.transform.position = new Vector3(xstep, ystep, zstep);
 
-
     }
-  
 
+    private bool CheckForController(bool currentValue)
+    {
+       bool controllerCheck= OVRInput.IsControllerConnected(OVRInput.Controller.RTrackedRemote) || OVRInput.IsControllerConnected(OVRInput.Controller.LTrackedRemote);
+
+        if(currentValue == controllerCheck)
+        {
+            return currentValue;
+        }
+        onHasController?.Invoke(currentValue);
+        return currentValue;
+    }
     void Update()
     {
-        world = GameObject.FindWithTag("Player");
-        if (Input.GetButtonDown("Fire1")) {
-            Camera.main.transform.position = new Vector3(xstep, ystep, zstep++);
+        if (!inputActive) {
+            return;
         }
-        if (OVRInput.Get(OVRInput.RawButton.DpadUp))
+        hasController = CheckForController(hasController);
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger)) {
+            onTriggerDown?.Invoke();
+
+        }
+        if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger))
         {
-            world.transform.position = new Vector3(xstep, ystep, zstep++);
+            onTriggerUp?.Invoke();
+
+        }
+        if (OVRInput.GetUp(OVRInput.Button.PrimaryTouchpad))
+        {
+            onTuchpadUp?.Invoke();
+
+        }
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryTouchpad))
+        {
+            onTuchpadDown?.Invoke();
+        }
+        float triggerValue = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger);
+        if (triggerValue > 0.5f)
+        {
+
         }
 
-        if (OVRInput.Get(OVRInput.Button.DpadDown))
-        {
-            world.transform.position = new Vector3(xstep, ystep, zstep--);
-        }
-    
+    }
+    private void PlayerFound()
+    {
+        inputActive = true;
+    }
+    private void PlayerLost()
+    {
+        inputActive =false;
+
     }
 }
