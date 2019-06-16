3207c66977909538a138f53269f2206a7a9f1e3a
gbanm <gbanm06@gmail.com>
2019-06-16 02:53:02 +0200

    beginning

--- Assets/Script/AddCelestialObjectsToMap.cs
+++ Assets/Script/AddCelestialObjectsToMap.cs
@@ -10,25 +10,24 @@ using System.Linq;
 public class AddCelestialObjectsToMap : MonoBehaviour
 {
 
-   private Vector3 _playerHeadPos = Vector3.zero;
+    private Vector3 _playerHeadPos = Vector3.zero;
     public LongLat _location;
     public List<Star> _stars;
     void Start()
     {
-        StartCoroutine(GetRequest("http://ip-api.com/json"));
+        // StartCoroutine(GetRequest("http://ip-api.com/json"));
         StartCoroutine(GetStarMarkerRequest("http://exoplanethunter.com/api/Maps/StarMarkers"));
         //Todo find correct rotation 
         // RenderSettings.skybox.SetFloat("_Rotation", 40);
-   
+
 
         var solarsystem = new List<Star> {
-        
+
             new Star { Name = "Trappist 1", Coordinates = new Vector3(14.22f, 7.11f, 40f) },
             new Star { Name = "Proxima Centauri", Coordinates = new Vector3(13f, 11f, 40f) } };
-        var orbGlow = Resources.Load("OrbGlow", typeof(Material)) as Material;
 
-        var glowBig = Resources.Load("OrbGlowExtreme", typeof(Material)) as Material;
-     
+        var glowBig = Resources.Load("Planet_B", typeof(Material)) as Material;
+
         foreach (var star in solarsystem)
         {
             GenerateMarkers(star, glowBig, star.Name);
@@ -40,43 +39,46 @@ public class AddCelestialObjectsToMap : MonoBehaviour
     void Update()
     {
 
-    
+
         OVRNodeStateProperties.GetNodeStatePropertyVector3(UnityEngine.XR.XRNode.Head,
         NodeStatePropertyType.Position,
         OVRPlugin.Node.Head,
         OVRPlugin.Step.Render,
         out _playerHeadPos);
-     //Debug.Log("Head: " + _playerHeadPos);
+        //Debug.Log("Head: " + _playerHeadPos);
 
     }
 
-    void GenerateMarkers(Star star, Material material, string tag) {
+    void GenerateMarkers(Star star, Material material, string tag)
+    {
         var marker = new GameObject();
-        marker = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        marker = GameObject.CreatePrimitive(PrimitiveType.Sphere);
         marker.GetComponent<Renderer>().material = material;
         marker.transform.position = star.Coordinates;
-        marker.transform.localScale = new Vector3(5, 5, 5);
-        marker.name =tag;
-        GameObject text = new GameObject();
-        TextMesh t = text.AddComponent<TextMesh>();
-        t.text = star.Name;
-        t.fontSize = 14;
-        t.transform.localEulerAngles += new Vector3(0, 0, 0);
-        t.transform.localPosition += star.Coordinates;
+        marker.transform.localScale = new Vector3(1, 1, 1);
+        marker.name = tag;
+      //  GameObject text = new GameObject();
+      //  TextMesh t = text.AddComponent<TextMesh>();
+      //  t.text = star.Name;
+      //  t.fontSize = 10;
+      //  t.transform.localEulerAngles += new Vector3(90, 0, 0);
+      //  t.transform.localPosition += star.Coordinates;
+
+
     }
 
     IEnumerator GetRequest(string uri)
     {
         UnityWebRequest uwr = UnityWebRequest.Get(uri);
         yield return uwr.SendWebRequest();
-        _location =  JsonConvert.DeserializeObject<LongLat>(uwr.downloadHandler.text);
+        _location = JsonConvert.DeserializeObject<LongLat>(uwr.downloadHandler.text);
 
 
     }
 
     public Vector3 SphericalToCartesian(float radius, float polar, float elevation)
     {
-       var outCart = new Vector3();
+        var outCart = new Vector3();
         float a = radius * Mathf.Cos(Mathf.PI * elevation / 180);
         outCart.x = a * Mathf.Cos(Mathf.PI * polar / 180);
         outCart.y = radius * Mathf.Sin(Mathf.PI * elevation / 180);
@@ -88,9 +90,9 @@ public class AddCelestialObjectsToMap : MonoBehaviour
     {
         UnityWebRequest uwr = UnityWebRequest.Get(uri);
         yield return uwr.SendWebRequest();
-        _stars = JsonConvert.DeserializeObject<FeatureCollection>(uwr.downloadHandler.text).Features.Select(o=>new Star { Name=o.Properties.Name, Coordinates =  SphericalToCartesian(12, o.Geometry.Coordinates.First(), o.Geometry.Coordinates.Last())}).ToList();
+        _stars = JsonConvert.DeserializeObject<FeatureCollection>(uwr.downloadHandler.text).Features.Select(o => new Star { Name = o.Properties.Name, Coordinates = SphericalToCartesian(30, o.Geometry.Coordinates.First(), o.Geometry.Coordinates.Last()) }).ToList();
 
-        var orbGlow = Resources.Load("OrbGlow", typeof(Material)) as Material;
+        var orbGlow = Resources.Load("Sun", typeof(Material)) as Material;
         foreach (var star in _stars)
         {
             GenerateMarkers(star, orbGlow, "star");
