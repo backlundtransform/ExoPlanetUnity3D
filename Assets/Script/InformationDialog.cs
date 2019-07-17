369224261105263383b6b74aeed0f284e82581ac
gbanm <gbanm06@gmail.com>
2019-07-17 23:49:09 +0200

    InformationDialog

---
+++ Assets/Script/InformationDialog.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+namespace Assets.Script
+{
+    public class InformationDialog : MonoBehaviour
+    {
+
+
+        private void Awake()
+        {
+       
+        }
+
+        private void Start()
+        {
+           
+        }
+
+
+}
