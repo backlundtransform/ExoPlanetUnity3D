2fad291ae28cb11662a80d626f1d6ef58a34ac7f
gbanm <gbanm06@gmail.com>
2019-09-08 01:07:24 +0200

    behavior of unity

--- Assets/Script/InformationDialog.cs
+++ Assets/Script/InformationDialog.cs
@@ -14,62 +14,64 @@ namespace Assets.Script
     public class InformationDialog : MonoBehaviour
     {
         private readonly string _url = "https://exoplanethunter.com/api/";
-     
+
         private TextContainer m_TextContainer;
 
         public void Start()
         {
-       
+
 
         }
 
         private void Awake()
         {
             var planetname = PlayerPrefs.GetString("PlanetId");
-     
-           StartCoroutine(GetExoPlanetByName($"{_url}ExoSolarSystems/ExoPlanets?%24filter=Name%20eq%20%27{HttpUtility.UrlEncode(planetname)}%27"));
-        
+
+            StartCoroutine(GetExoPlanetByName($"{_url}ExoSolarSystems/ExoPlanets?%24filter=Name%20eq%20%27{HttpUtility.UrlEncode(planetname)}%27"));
+
         }
 
         private void Update()
         {
-       
+
             if (OVRInput.GetDown(OVRInput.Button.Back))
             {
                 SceneManager.LoadScene("PlanetSystem", LoadSceneMode.Single);
 
             }
-       
+
 
 
         }
         private IEnumerator GetExoPlanetByName(string uri)
         {
-           UnityWebRequest uwr = UnityWebRequest.Get(uri);
+            UnityWebRequest uwr = UnityWebRequest.Get(uri);
             yield return uwr.SendWebRequest();
-        
-            
-                var planet = JsonConvert.DeserializeObject<List<Planet>>(uwr.downloadHandler.text).First();
-             
 
-            var infoarray = new Dictionary<string,string>();
-            infoarray.AddIfNotNull("Radius",$"{ planet.RadiusEu}*Earth", planet.RadiusEu);
-            infoarray.AddIfNotNull("Mass",$"{planet.Mass}*Earth", planet.Mass);
-            infoarray.AddIfNotNull("Gravity",$"{planet.Gravity}*Earth", planet.Gravity);
-            infoarray.AddIfNotNull("Surface Pressure",$"{planet.SurfacePressure}*Earth", planet.SurfacePressure);
-            infoarray.AddIfNotNull("Escape Velocity",$"{planet.EscapeVelocity}*Earth", planet.EscapeVelocity);
 
-            var win = Screen.width * 0.6f;
-            var w1 = win * 0.35f; var w2 = win * 0.35f;
+            var planet = JsonConvert.DeserializeObject<List<Planet>>(uwr.downloadHandler.text).First();
 
-            //find out how dispalay a table in vr World Space UI
-            foreach (var item in infoarray)
+
+            var infoarray = new List<string>();
+            infoarray.AddIfNotNull($"Radius {planet.RadiusEu}*Earth", planet.RadiusEu);
+            infoarray.AddIfNotNull($"Mass {planet.Mass}*Earth", planet.Mass);
+            infoarray.AddIfNotNull($"Gravity {planet.Gravity}*Earth", planet.Gravity);
+            infoarray.AddIfNotNull($"Surface Pressure {planet.SurfacePressure}*Earth", planet.SurfacePressure);
+            infoarray.AddIfNotNull($"Escape Velocity {planet.EscapeVelocity}*Earth", planet.EscapeVelocity);
+
+
+
+            for (var i = 0; i < infoarray.Count - 1; i++)
             {
-                GUILayout.BeginHorizontal();
-                GUILayout.Label(item.Key, GUILayout.Width(w1));
-                GUILayout.Label(item.Value, GUILayout.Width(w2));
-                GUILayout.EndHorizontal();
 
+                var m_Text = GetComponent<TextMeshPro>() ?? gameObject.AddComponent<TextMeshPro>();
+
+                m_TextContainer = GetComponent<TextContainer>();
+
+                m_Text.fontSize = 8;
+                m_Text.transform.localPosition = new Vector3(i, 0, 8);
+
+                m_Text.text = infoarray[i];
             }
 
         }
